17d8c26220094ebbceb6b985fd644fa0adb5257f
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2022-04-08 23:54:57 +0900

    不要ファイルの削除

--- bin/os.ts
+++
@@ -1,3 +0,0 @@
-exports.is_windows = process.platform==='win32'
-exports.is_mac = process.platform==='darwin'
-exports.is_linux = process.platform==='linux'
--- src/api/v1/blogs/_id@string/comments/@types.ts
+++
@@ -1,9 +0,0 @@
-export type CommentsSchema = {
-    [id: string]: {
-        title: string;
-        content: string;
-        createdAt: string;
-        updatedAt: string;
-        userId: string | null;
-    }
-}
