87a7e805ea609de19de49a76f9b5855a78934fbf
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2022-04-16 00:53:55 +0900

    デフォルトイメージ(画像が定義されていないとデフォルトで表示する画像)を定義

---
+++ src/const/images/DefaultImage/index.ts
@@ -0,0 +1,9 @@
+import Twitter from '@/assets/twitter-icon.png';
+
+const DefaultImage = {
+    src: Twitter.src,
+    width: undefined,
+    height: undefined
+}
+
+export default DefaultImage
---
+++ src/const/images/index.ts
@@ -0,0 +1,5 @@
+import DefaultImage from "./DefaultImage";
+
+export {
+    DefaultImage
+}
