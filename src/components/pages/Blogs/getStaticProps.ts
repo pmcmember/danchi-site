0b8b3e8ff902f5d96c596601a4a2a45b21a576f6
shigeyuki-nakano <shigeyukinakano0602@gmail.com>
2022-08-23 12:41:20 +0900

    リファクタ

--- src/components/pages/Blogs/getStaticProps.ts
+++ src/components/pages/Blogs/getStaticProps.ts
@@ -1,27 +1,28 @@
 import { GetStaticProps } from 'next'
-import { client } from '@/lib/aspida';
+import { client } from '@/lib/aspida'
 import { BlogsResultList } from '@/api/@types'
-import { Params, PER_PAGE } from './getStaticPaths';
+import { Params, PER_PAGE } from './getStaticPaths'
+import { PaginationProps } from '@/const/pages'
 
 export type Props = {
-    blogs: BlogsResultList;
-    currentPage: string;
-    totalPages: string;
-}
+    blogs: BlogsResultList
+} & PaginationProps
 
-export const getStaticProps: GetStaticProps<Props, Params> = async ({params}) => {
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+    params,
+}) => {
     const result: BlogsResultList = await client.v1.blogs.$get({
         query: {
             limit: PER_PAGE,
-            offset: (Number(params?.pid || 1) - 1) * PER_PAGE 
-        }
-    });
+            offset: (Number(params?.pid || 1) - 1) * PER_PAGE,
+        },
+    })
 
     return {
         props: {
             blogs: result,
             currentPage: String(params?.pid || 1),
-            totalPages: String(result.totalCount || 1)
+            totalPages: String(result.totalCount || 1),
         },
     }
 }
