f3428a5341b431e67a40dd2dad0bf053150fd449
shigeyuki-nakano <shigeyukinakano0602@gmail.com>
2022-09-12 10:13:23 +0900

    共通レイアウトの変更

--- src/components/ui/GlobalLayout/Header/Header.tsx
+++ src/components/ui/GlobalLayout/Header/Header.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from 'react'
 
-import styles from "./Header.module.css"
+import styles from './Header.module.css'
 
 import { PageIcons, PageNames, PageTitles } from '@/const/pages'
-import Link from 'next/link';
-import { useRouter }from 'next/router';
-
-
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Image } from '@/components/ui/Image'
+import Logo from '@/assets/danchi-logo.png'
 
 type Props = {
-    style?: React.CSSProperties;
-    className?: string;
+    style?: React.CSSProperties
+    className?: string
     isHide: boolean
 }
 
-export const Header: React.VFC<Props> = ({style, className, isHide}) => {
-    const currentPage = useRouter().pathname.split("/")[1] || "top";
+export const Header: React.VFC<Props> = ({ style, className, isHide }) => {
+    const currentPage = useRouter().pathname.split('/')[1] || 'top'
 
     return (
         <header
-            style={{transform: `${isHide ? "translateY(-150%)" : "translateY(0)"}`, transition: ".4s"}}
-            className={`${styles.header} ${className || ""}`}
+            style={{
+                transform: `${isHide ? 'translateY(-150%)' : 'translateY(0)'}`,
+                transition: '.4s',
+            }}
+            className={`${styles.header} ${className || ''}`}
         >
             <div className={styles.content}>
                 {/* <div className={styles.content__left}>
@@ -28,27 +31,35 @@ export const Header: React.VFC<Props> = ({style, className, isHide}) => {
                 </div> */}
                 <h1 className={styles.content__center}>
                     <Link href="/">
-                        <a>DANCHi</a>
+                        <a className="relative">
+                            <Image className="w-full" src={Logo} />
+                            <span className={Logo ? 'opacity-0' : ''}>
+                                DANCHi
+                            </span>
+                        </a>
                     </Link>
                 </h1>
-                <div className={styles.content__right}>
-                </div>
             </div>
             <div className="">
                 <nav className="rmd:overflow-x-scroll">
                     <ul className={`flex items-center`}>
-                        {Array.from(PageTitles.keys())
-                        .map((page) => (
+                        {Array.from(PageTitles.keys()).map((page) => (
                             <li
                                 className={`text-center flex-shrink-0 flex-grow bg-white`}
                                 key={page}
                             >
                                 <Link href={`/${page}`}>
-                                    <a className={`${currentPage === page?"bg-slate-50" : ""} hover:bg-slate-100 duration-300 w-full px-5 border-x border-x-slate-300 flex justify-center items-center`}>
+                                    <a
+                                        className={`${
+                                            currentPage === page
+                                                ? 'bg-slate-50'
+                                                : ''
+                                        } hover:bg-slate-100 duration-300 w-full px-5 border-x border-x-slate-300 flex justify-center items-center`}
+                                    >
                                         <span className="mr-2">
                                             {(() => {
-                                                const Icon = PageIcons.get(page);
-                                                return Icon? <Icon/>: "";
+                                                const Icon = PageIcons.get(page)
+                                                return Icon ? <Icon /> : ''
                                             })()}
                                         </span>
                                         {PageTitles.get(page)}
--- src/components/ui/GlobalLayout/Main/Main.tsx
+++ src/components/ui/GlobalLayout/Main/Main.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 import styles from './Main.module.css'
-import BgImage from '@/assets/Brick01-p.jpg'
 
 type Props = {
-    children: React.ReactNode;
-    style?: React.CSSProperties;
-    className?: string;
-};
+    children: React.ReactNode
+    style?: React.CSSProperties
+    className?: string
+}
 
-export const Main: React.VFC<Props> = ({children, style, className}) => {
+export const Main: React.VFC<Props> = ({ children, style, className }) => {
     return (
-        <div
-            style={style}
-            className={`${styles.house} ${className || ""}`}
-        >
-            <div className={`${styles.house__body}`} style={{backgroundImage: `url(${BgImage.src})`, backgroundSize: "7%"}}>
-                <main className={styles.house__body__content} style={{background: "transparent"}}>
+        <div style={style} className={`${styles.house} ${className || ''}`}>
+            <div className={`${styles.house__body}`}>
+                <main
+                    className={styles.house__body__content}
+                    style={{ background: 'transparent' }}
+                >
                     {children}
                 </main>
             </div>
