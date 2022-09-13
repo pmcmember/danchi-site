552e0fad80576b70f89734337174816f08191761
shigeyuki-nakano <shigeyukinakano0602@gmail.com>
2022-09-14 01:24:38 +0900

    画像コンポーネントの修正

--- src/components/ui/Image/Image.tsx
+++ src/components/ui/Image/Image.tsx
@@ -2,15 +2,25 @@ import React from 'react'
 import NextImage, { ImageProps } from 'next/image'
 import DefaultImage from '@/assets/default-image.png'
 
-type Props = Omit<ImageProps, 'layout'>
+type Props = Omit<ImageProps, 'layout' | 'src'> & { src?: ImageProps['src'] }
 
 export const Image: React.FC<Props> = (props) => {
+    const src: string =
+        typeof props.src === 'string' ? props.src : (props.src as any).src
     return (
-        <NextImage
+        <img
             {...props}
-            src={props.src || DefaultImage}
-            className="object-cover"
-            layout="fill"
+            src={src || DefaultImage.src}
+            className={`object-cover absolute top-0 left-0 w-full h-full ${
+                props.className || ''
+            }`}
+            // layout="fill"
         />
+        // <NextImage
+        //     {...props}
+        //     src={props.src || DefaultImage}
+        //     className={`object-cover ${props.className || ''}`}
+        //     layout="fill"
+        // />
     )
 }
