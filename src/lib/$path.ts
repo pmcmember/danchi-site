87b265f786b071f1997c1217ae0b77e21fb1776f
shigeyuki-nakano <shigeyukinakano0602@gmail.com>
2022-09-14 01:27:32 +0900

    ライブラリ共通定義の修正

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -17,20 +17,12 @@ export const pagesPath = {
     $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
   },
   musics: {
-    categories: {
-      _category: (category: string | number) => ({
-        $url: (url?: { hash?: string }) => ({ pathname: '/musics/categories/[category]' as const, query: { category }, hash: url?.hash })
-      })
-    },
     $url: (url?: { hash?: string }) => ({ pathname: '/musics' as const, hash: url?.hash })
   },
   top: {
     $url: (url?: { hash?: string }) => ({ pathname: '/top' as const, hash: url?.hash })
   },
   videos: {
-    _pid: (pid: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/videos/[pid]' as const, query: { pid }, hash: url?.hash })
-    }),
     $url: (url?: { hash?: string }) => ({ pathname: '/videos' as const, hash: url?.hash })
   },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
--- src/lib/aspida/client.ts
+++ src/lib/aspida/client.ts
@@ -1,14 +1,14 @@
-import api from '@/api/$api';
-import aspida, { FetchConfig } from '@aspida/fetch';
-
+import api from '@/api/$api'
+import aspida, { FetchConfig } from '@aspida/fetch'
+import { apiConfig } from '@/const/api'
 
 const fetchConfig: FetchConfig = {
     headers: {
-        "X-Custom-Authorization": "Bearer " + process.env.NEXT_PUBLIC_API_AUTHORIZATION_KEY || "",
-        "X-Api-Key": process.env.NEXT_PUBLIC_API_KEY || ""
+        'X-Custom-Authorization': 'Bearer ' + apiConfig.authorizationKey,
+        'X-Api-Key': apiConfig.apiKey,
     },
-    baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
-    mode: "cors"
+    baseURL: apiConfig.apiBaseUrl,
+    mode: 'cors',
 }
 
 export const client = api(aspida(fetch, fetchConfig))
