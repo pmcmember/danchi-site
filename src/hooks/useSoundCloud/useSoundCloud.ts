0085e8942b1c219ffe35240db65443197a71b13a
shigeyuki-nakano <shigeyukinakano0602@gmail.com>
2022-09-14 01:26:50 +0900

    サウンドクラウドのエラーハンドリング修正

--- src/hooks/useSoundCloud/useSoundCloud.ts
+++ src/hooks/useSoundCloud/useSoundCloud.ts
@@ -1,75 +1,76 @@
-import React from 'react';
+import React from 'react'
 import {
     UseSoundCloud,
     SoundCloudStatus,
     RequestNextSong,
-    OnPlayButtonClick
+    OnPlayButtonClick,
 } from './useSoundCloud.type'
 
-
-
-
-export const useSoundCloud: UseSoundCloud = ({iframeId}) => {
-    const [status, setStatus] = React.useState<SoundCloudStatus>("READY");
-    const [widget, setWidget] = React.useState<any>();
+export const useSoundCloud: UseSoundCloud = ({ iframeId }) => {
+    const [status, setStatus] = React.useState<SoundCloudStatus>('READY')
+    const [widget, setWidget] = React.useState<any>()
 
     // windowオブジェクトが読み込まれたら実行
     React.useEffect(() => {
         try {
-            console.log("useSoundCloud: init")
-            const SoundCloudWidget = require("soundcloud-widget");
+            console.log('useSoundCloud: init')
+            const SoundCloudWidget = require('soundcloud-widget')
             const widget = new SoundCloudWidget(iframeId)
-            
-            // setSCEvents(SoundCloudWidget.events);
-            setWidget(widget);
+
+            setWidget(widget)
 
             // イベントのバインド
             Object.keys(SoundCloudWidget.events).map((event) => {
                 const _event = event as SoundCloudStatus
-                
+
                 /**
-                 * widgetAPIのイベント変更に対するバインド: 
+                 * widgetAPIのイベント変更に対するバインド:
                  *      サウンドクラウドイベントが変更する際にstatusステートも変更するようにバインド
                  */
                 widget.bind(SoundCloudWidget.events[_event], () => {
                     setStatus(_event)
                 })
             })
-        } catch(e) {
-            console.log("useSoundCloud: error")
-            setStatus("ERROR");
+        } catch (e) {
+            console.log('useSoundCloud: error')
+            setStatus('ERROR')
         }
-    }, [iframeId]);
+    }, [iframeId])
 
-
-    const onPlayButtonClick: OnPlayButtonClick = React.useCallback(() => {
-        if( ! widget) {
-            setStatus("ERROR")
-            return Promise.resolve();
+    const onPlayButtonClick: OnPlayButtonClick = React.useCallback(async () => {
+        if (!widget) {
+            setStatus('ERROR')
+            return
         }
 
-        return Promise.resolve(widget.toggle())
-            .catch(() => {
-                setStatus("ERROR")
-            });
+        try {
+            widget.toggle()
+        } catch (e) {
+            console.error(e)
+            setStatus('ERROR')
+        }
     }, [widget, status, setStatus])
 
-    const requestNextSong: RequestNextSong = React.useCallback((url: string) => {
-        if( ! widget) {
-            setStatus("ERROR")
-            return Promise.resolve();
-        }
-        
-        return widget.load(url)
-        .catch(() => {
-            setStatus("ERROR");
-        })
-    }, [widget, setStatus])
+    const requestNextSong: RequestNextSong = React.useCallback(
+        async (url: string) => {
+            if (!widget) {
+                setStatus('ERROR')
+                return
+            }
+
+            try {
+                return widget.load(url)
+            } catch (e) {
+                console.error(e)
+                setStatus('ERROR')
+            }
+        },
+        [widget, setStatus]
+    )
 
     return {
         onPlayButtonClick,
         requestNextSong,
-        soundCloudStatus: status
-        // soundCloudStatus: "SEEK"
+        soundCloudStatus: status,
     }
 }
