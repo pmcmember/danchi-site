aca4fcfa187980a145c0af4c58351706c33b4cb2
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2023-02-13 00:45:09 +0900

    リファクタ

--- src/components/ui/UIs/PlaySongUI/PlaySongUI.tsx
+++ src/components/ui/UIs/PlaySongUI/PlaySongUI.tsx
@@ -16,10 +16,10 @@ export const PlaySongUI: React.VFC<Props> = ({ soundCloudContents }) => {
     const iframeId = 'play-song-ui'
     const { onPlayButtonClick, requestNextSong, soundCloudStatus } =
         useSoundCloud({ iframeId })
-    const { setIsModalOpen, Modal } = useModal({})
     const [currentSong, setCurrentSong] = React.useState<
         MusicListResponse['contents'][0]
     >(soundCloudContents[Math.floor(Math.random() * soundCloudContents.length)])
+    const { setIsModalOpen, Modal } = useModal({})
 
     const onPlayButtonClickHandler = React.useCallback(() => {
         soundCloudStatus === 'PAUSE' || soundCloudStatus === 'SEEK'
@@ -30,42 +30,32 @@ export const PlaySongUI: React.VFC<Props> = ({ soundCloudContents }) => {
                       ]
 
                   setCurrentSong(scInfo)
-                  requestNextSong(scInfo.scApiUrl || '').then(() => {
-                      onPlayButtonClick()
-                  })
+                  requestNextSong(scInfo.scApiUrl || '').then(onPlayButtonClick)
               })()
-            : (() => {
-                  onPlayButtonClick()
-              })()
-    }, [soundCloudStatus, setCurrentSong, requestNextSong, onPlayButtonClick])
+            : onPlayButtonClick()
+    }, [soundCloudStatus])
 
     React.useEffect(() => {
-        if (soundCloudStatus === 'FINISH') {
-            const currentSongIndex = soundCloudContents.findIndex(
-                (c) => c.id === currentSong.id
-            )
-
-            const nextSong =
-                currentSongIndex + 1 >= soundCloudContents.length ||
-                currentSongIndex === -1
-                    ? soundCloudContents[0]
-                    : soundCloudContents[currentSongIndex + 1]
-
-            if (!nextSong.scApiUrl) return
-
-            setCurrentSong(nextSong)
-
-            requestNextSong(nextSong.scApiUrl).then(() => {
-                onPlayButtonClick()
-            })
+        if (soundCloudStatus !== 'FINISH') {
+            return
         }
-    }, [
-        soundCloudStatus,
-        requestNextSong,
-        onPlayButtonClick,
-        currentSong,
-        setCurrentSong,
-    ])
+
+        const currentSongIndex = soundCloudContents.findIndex(
+            (c) => c.id === currentSong.id
+        )
+
+        const nextSong =
+            currentSongIndex + 1 >= soundCloudContents.length ||
+            currentSongIndex === -1
+                ? soundCloudContents[0]
+                : soundCloudContents[currentSongIndex + 1]
+
+        if (!nextSong.scApiUrl) return
+
+        setCurrentSong(nextSong)
+
+        requestNextSong(nextSong.scApiUrl).then(onPlayButtonClick)
+    }, [soundCloudStatus, currentSong])
 
     return (
         <>
@@ -106,57 +96,66 @@ export const PlaySongUI: React.VFC<Props> = ({ soundCloudContents }) => {
                 />
             </div>
             <Modal>
-                <Box className="bg-white w-10/12 p-8 rounded-xl absolute">
-                    <div>
-                        <div className="font-bold text-2xl border-b border-slate-800 pb-3 mb-6">
-                            流れている曲詳細
-                        </div>
-                        <ul className="flex flex-col gap-4">
-                            <li className="flex gap-2 items-center">
-                                <FaUser className="text-2xl" />
-                                <a href={currentSong.scArtistHref}>
-                                    {currentSong.scArtistName}
-                                </a>
-                            </li>
-                            <li className="flex gap-2 items-center">
-                                <IoIosMusicalNotes className="text-2xl" />
-                                <a href={currentSong.scSongHref}>
-                                    {currentSong.scSongTitle}
-                                </a>
-                            </li>
-
-                            <ul className="flex gap-2">
-                                {currentSong.songGenres.value.map((genre) => (
-                                    <li
-                                        style={{ fontSize: '10px' }}
-                                        key={`${currentSong.id}-${genre}`}
-                                    >
-                                        <div className="rounded-full bg-slate-500 text-white px-2 py-1">
-                                            {`#${genre}`}
-                                        </div>
-                                    </li>
-                                ))}
-                                {currentSong.songImages.value.map((image) => (
-                                    <li
-                                        style={{ fontSize: '10px' }}
-                                        key={`${currentSong.id}-${image}`}
-                                    >
-                                        <div className="rounded-full bg-slate-500 text-white px-2 py-1">
-                                            {`#${image}`}
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </ul>
-                    </div>
-                    <div
-                        onClick={() => setIsModalOpen(false)}
-                        className="cursor-pointer mx-auto mt-10 rounded-xl text-center border border-slate-800 w-full py-3"
-                    >
-                        閉じる
-                    </div>
-                </Box>
+                <ModalContent
+                    song={currentSong}
+                    closeModal={() => setIsModalOpen(false)}
+                />
             </Modal>
         </>
+    )
+}
+
+type ModalContent = {
+    song: MusicListResponse['contents'][0]
+    closeModal: () => void
+}
+const ModalContent: React.FC<ModalContent> = ({ song, closeModal }) => {
+    return (
+        <Box className="bg-white w-10/12 p-8 rounded-xl absolute">
+            <div>
+                <div className="font-bold text-2xl border-b border-slate-800 pb-3 mb-6">
+                    流れている曲詳細
+                </div>
+                <ul className="flex flex-col gap-4">
+                    <li className="flex gap-2 items-center">
+                        <FaUser className="text-2xl" />
+                        <a href={song.scArtistHref}>{song.scArtistName}</a>
+                    </li>
+                    <li className="flex gap-2 items-center">
+                        <IoIosMusicalNotes className="text-2xl" />
+                        <a href={song.scSongHref}>{song.scSongTitle}</a>
+                    </li>
+
+                    <ul className="flex gap-2">
+                        {song.songGenres.value.map((genre) => (
+                            <li
+                                style={{ fontSize: '10px' }}
+                                key={`${song.id}-${genre}`}
+                            >
+                                <div className="rounded-full bg-slate-500 text-white px-2 py-1">
+                                    {`#${genre}`}
+                                </div>
+                            </li>
+                        ))}
+                        {song.songImages.value.map((image) => (
+                            <li
+                                style={{ fontSize: '10px' }}
+                                key={`${song.id}-${image}`}
+                            >
+                                <div className="rounded-full bg-slate-500 text-white px-2 py-1">
+                                    {`#${image}`}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </ul>
+            </div>
+            <div
+                onClick={() => closeModal()}
+                className="cursor-pointer mx-auto mt-10 rounded-xl text-center border border-slate-800 w-full py-3"
+            >
+                閉じる
+            </div>
+        </Box>
     )
 }
